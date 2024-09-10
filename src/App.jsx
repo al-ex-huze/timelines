6aa77d94c691084ff891f514b084e6ef41d0269e
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-10 20:32:21 +0100

    implements basic app bar top

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import AppBarTop from "./components/AppBarTop";
+
 const App = () => {
-    return <CssBaseline />;
+    return (
+        <>
+            <CssBaseline />
+            <AppBarTop />
+        </>
+    );
 };
 
 export default App;
