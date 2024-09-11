aca1a2003576995eac742b3f34f2e383eab66e39
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-11 17:04:30 +0100

    creates side drawer component

--- src/components/DrawerController.jsx
+++ src/components/DrawerController.jsx
@@ -1,57 +1,8 @@
 import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
-import MuiDrawer from "@mui/material/Drawer";
+
+import SideDrawer from "./SideDrawer";
 import BottomDrawer from "./BottomDrawer";
-import SideDrawerContents from "./SideDrawerContents";
-import { isMobile } from "react-device-detect";
 
-const drawerWidth = 240;
-
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: "hidden",
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up("sm")]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
-});
-
-const SideDrawer = styled(MuiDrawer, {
-    shouldForwardProp: (prop) => prop !== "open",
-})(({ theme }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap",
-    boxSizing: "border-box",
-    variants: [
-        {
-            props: ({ open }) => open,
-            style: {
-                ...openedMixin(theme),
-                "& .MuiDrawer-paper": openedMixin(theme),
-            },
-        },
-        {
-            props: ({ open }) => !open,
-            style: {
-                ...closedMixin(theme),
-                "& .MuiDrawer-paper": closedMixin(theme),
-            },
-        },
-    ],
-}));
 
 const DrawerController = ({
     open,
@@ -59,19 +10,7 @@ const DrawerController = ({
     forceMobile,
     openMobileBottom,
     setOpenMobileBottom,
-    toggleBottomDrawer,
 }) => {
-    const theme = useTheme();
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-        console.log(open);
-    };
-
     return (
         <>
             {forceMobile ? (
@@ -82,17 +21,9 @@ const DrawerController = ({
                 </BottomDrawer>
             ) : (
                 <SideDrawer
-                    variant="permanent"
-                    open={open}
-                    ModalProps={{
-                        keepMounted: true,
-                    }}
+                open={open}
+                setOpen={setOpen}
                 >
-                    <SideDrawerContents
-                        open={open}
-                        handleDrawerOpen={handleDrawerOpen}
-                        handleDrawerClose={handleDrawerClose}
-                    />
                 </SideDrawer>
             )}
         </>
